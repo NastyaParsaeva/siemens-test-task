8500f9fa1048bffcd751aa550b6e1d99cf8554ab
Anastasia Parsaeva <nast.ovc@yandex.ru>
2021-06-21 12:56:59 +0400

    basic chart

---
+++ src/components/Graph/Graph.tsx
@@ -0,0 +1,74 @@
+import { scaleLinear } from "d3-scale";
+import { line } from "d3-shape";
+import {
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from "react";
+import { Dot } from "../../App";
+import "./Graph.css";
+
+export function Graph({ dots }: { dots: Dot[] }) {
+  const [size, setSize] = useState({ width: 0, height: 0 });
+  const [d, setD] = useState('');
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize({ width: window.innerWidth, height: window.innerHeight });
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
+  // d3.select(window).on('resize', resize);
+
+  console.log(dots);
+  const times: number[] = dots.map((item: any) =>item.time.valueOf());
+  const values: number[] = dots.map((item: any) => item.value);
+
+  const maxTime: number = Math.max(...times);
+  const minTime = Math.min(...times);
+  const maxValue: number = Math.max(...values);
+  const minValue: number = Math.min(...values);
+
+  useEffect(() => {
+    const xScale = scaleLinear()
+      .domain([minTime, maxTime])
+      .range([0, size.width - 300]);
+
+    const yScale = scaleLinear()
+      .domain([maxValue, minValue])
+      .range([0, size.height - 60]);
+
+    const myLine = line()
+      .x((d: any) => {
+        return xScale(d[0]);
+      })
+      .y((d: any) => yScale(d[1]));
+
+    setD(myLine(
+      dots.map((item) => {
+        return [item.time.valueOf(), item.value];
+      })
+    ) as string);
+  }, [size, dots, maxTime, maxValue, minTime])
+  
+  return (
+    <div className="graph-container">
+      <svg width={"100%"} height={size.height - 30}>
+        <path
+          className="line"
+          fill="none"
+          stroke="steelblue"
+          strokeWidth="1.5"
+          strokeLinejoin="round"
+          strokeLinecap="round"
+          preserveAspectRatio="xMinYMin meet"
+          viewBox="0 0 300 300"
+          d={d}
+        />
+      </svg>
+    </div>
+  );
+}
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,64 @@
+import debounce from "lodash.debounce";
+import moment from "moment";
+import { ChangeEvent, ChangeEventHandler, KeyboardEvent, MouseEventHandler, useCallback, useState } from "react";
+import { Dot } from "../../App";
+import "./Sidebar.css";
+// import { debounce } from 'lodash.debounce';
+
+export function Sidebar({
+    dots,
+    deleteDot,
+    createDot,
+  }: {
+    dots: Dot[],
+    deleteDot: (index: number) => void,
+    createDot: (dot: Dot) => void,
+  }) {
+    const [inputValue, setInputValue] = useState('');
+
+    // const debouncedSave = useCallback(
+    //   debounce(nextValue => {
+    //     createDot({
+    //       time: moment(),
+    //       value: nextValue
+    //     });
+    //     setInputValue('');
+    //   }, 1000),
+    // [inputValue]);
+  
+    const handleChange = (event: ChangeEvent) => {
+      const nextValue = (event.target as any).value;
+      setInputValue(nextValue);
+      // debouncedSave(nextValue);
+    };
+
+    const handleKeyPress = (event: KeyboardEvent)=> {
+      if (event.key === 'Enter') {
+        createDot({
+          time: moment(),
+          value: Number(inputValue),
+        });
+        setInputValue('');
+      }
+    }
+
+  return (
+    <div className="sidebar">
+      <p className="sidebar__title">Data</p>
+      <input
+        className="sidebar__input"
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+        value={inputValue}
+      />
+      <p className="sidebar__values-title">List of values</p>
+      {dots.map((item, index) => (
+        <div key={item.time.toString()} className="sidebar-item">
+          <span className="sidebar-item__time">{item.time.format('mm:ss:SSS')}</span>
+          <span className="sidebar-item__value">{item.value}</span>
+          <button className="sidebar-item__remove" onClick={() => deleteDot(index)}>Remove</button>
+        </div>
+      ))}
+    </div>
+  );
+}
